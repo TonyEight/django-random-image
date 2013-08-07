f07f37d0eab5d59e812235b4737918c082089410
TonyEight <ludovic.legendart@gmail.com>
2013-08-08 01:21:24 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+from setuptools import setup, find_packages
+ 
+import ramdom_image
+ 
+
+setup(
+ 
+    name='django_random_image',
+ 
+    version=ramdom_image.__version__,
+ 
+    packages=find_packages(),
+ 
+    author="TonyEight",
+ 
+    author_email="ludoic.legendart@gmail.com",
+ 
+    description="A simple tag that get you a random image URL from the specified directory under STATIC ROOT",
+
+    long_description=open('README.rst').read(),
+ 
+    include_package_data=True,
+
+    url='https://github.com/TonyEight/django-random-image',
+ 
+    classifiers=[
+        "Programming Language :: Python",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Django Template Tag",
+    ],
+ 
+)
