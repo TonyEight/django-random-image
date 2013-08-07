aa346d415af27a91e8d7be741b2acef9176e6eec
TonyEight <ludovic.legendart@gmail.com>
2013-08-08 01:03:52 +0200

    Update random_image.py
    Make the path relative to STATIC_ROOT

--- random_image/templatetags/random_image.py
+++ random_image/templatetags/random_image.py
@@ -16,7 +16,7 @@ def random_image(image_dir):
         rel_dir = image_dir
     else:
         rel_dir = settings.RANDOM_IMAGE_DIR
-    rand_dir = os.path.join(settings.MEDIA_ROOT, rel_dir)
+    rand_dir = os.path.join(settings.STATIC_ROOT, rel_dir)
 
     files = [f for f in os.listdir(rand_dir) if os.path.splitext(f)[1] in valid_extensions]
 
